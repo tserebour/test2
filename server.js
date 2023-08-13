a38889a0388859261b900761eb4e12aed884de5b
tserebour <tserebour1@gmail.com>
2023-08-13 01:50:31 +0100

    2

--- server.js
+++ server.js
@@ -6,15 +6,30 @@
 'use strict';
 
 const express = require('express');
+const { ethers, BigNumber } = require("ethers");
+const provider = new ethers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/cIP70Q4bVNMcB-XDUp2k7griYEbmW7li');
+
+
+
+
+
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+
+
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello remote wold!\n');
+	provider.getBalance('0x003150255744a88946bE2190cc3214C1aeA9aA89').then((result) => {
+		console.log(result);
+
+		res.send("Current balance : " + result);
+		
+	});
+	
 });
 
 app.listen(PORT, HOST);
